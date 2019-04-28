bf8d36899b259d642601762510454da64e8dd2db
github username <github email address>
2019-04-28 13:39:35 -0700

    modified auth controller and menu items

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,9 +2,7 @@ class Api::V1::AuthController < ApplicationController
   skip_before_action :authorized, only: [:create]
 
   def create
-    
     @user = User.find_by(username: user_login_params[:username])
-
     if @user && @user.authenticate(user_login_params[:password])
       # encode token comes from ApplicationController
       token = encode_token({ user_id: @user.id })
--- app/controllers/api/v1/menu_items_controller.rb
+++ app/controllers/api/v1/menu_items_controller.rb
@@ -2,16 +2,11 @@ class Api::V1::MenuItemsController < ApplicationController
   skip_before_action :authorized
 
   def index
-    # byebug
-    # # @user = User.find(params[:user_id])
-    # # @items = @user.menu_items
-    # # render json: @items
     @items =MenuItem.all
     render json: @items
   end
 
   def create
-    # byebug
     @user = User.find(menu_items_params[:user_id])
     @items = MenuItem.new(menu_items_params)
     @items.user_id = @user.id
@@ -20,13 +15,11 @@ class Api::V1::MenuItemsController < ApplicationController
   end
 
   def destroy
-    # byebug
     @item = MenuItem.find(params[:id])
     @item.delete
   end
 
   private
-
 
   def menu_items_params
     params.require(:menu_item).permit(:name, :description, :price, :img_url, :user_id, :category, :menu_item)
